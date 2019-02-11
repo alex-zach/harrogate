799b2e1edd0c771d7e70eceef2fc82fce0f2cf53
Alexander Zach <alexander.zach1@gmail.com>
2019-02-11 18:39:00 +0100

    Fixed Error with node 11.9

--- shared/scripts/workspace-manager.js
+++ shared/scripts/workspace-manager.js
@@ -45,7 +45,7 @@ WorkspaceManager = (function() {
 
   WorkspaceManager.prototype.update_workspace_path = function(path) {
     this.workspace_path = path;
-    Fs.writeFile(this.workspace_file_path, JSON.stringify(this.workspace_path, null, 2), 'utf8');
+    Fs.writeFile(this.workspace_file_path, JSON.stringify(this.workspace_path, null, 2), 'utf8', () => {});
   };
 
   return WorkspaceManager;
